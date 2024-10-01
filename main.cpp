cba6b0e3b2981b1e712878e7a0fdf51058492604
MoRGeoS <167065261+MoRGeoS@users.noreply.github.com>
2024-10-01 15:24:05 -0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,16 @@
 #include <thread>
 #include <chrono>
 
+#include "Draw.h"
+
 int main()
 {
   std::thread render(Render);
   std::thread draw(Draw);
   while (isRunning)
     {
-      
+      //Your code if you need
     }
+  render.detach();
+  draw.detach();
 }
