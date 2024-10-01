55fd458a858595519d60d1a47f36d9212d049b61
MoRGeoS <167065261+MoRGeoS@users.noreply.github.com>
2024-10-01 15:21:01 -0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
 //Not Started Yet
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+int main()
+{
+  std::thread render(Render);
+  std::thread draw(Draw);
+  while (isRunning)
+    {
+      
+    }
+}
