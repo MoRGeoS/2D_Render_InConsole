5cb4a862bd17ce6bddba316ca88a30710ea4ef83
MoRGeoS <167065261+MoRGeoS@users.noreply.github.com>
2024-10-01 20:10:11 -0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,32 @@
 
 #include "Draw.h"
 
+void setConsole()
+{
+    //Under main Function
+}
+
 int main()
 {
-  std::thread render(Render);
-  std::thread draw(Draw);
-  while (isRunning)
+    setConsole();
+    std::thread render(Render);
+    std::thread draw(Draw);
+    std::thread fps(Fps);
+    while (isRunning)
     {
-      //Your code if you need
+        //Your code if you need one
     }
-  render.detach();
-  draw.detach();
+    fps.detach();
+    render.detach();
+    draw.detach();
+}
+
+void setConsole()
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    DWORD dwMode = 0;
+    GetConsoleMode(hConsole, &dwMode);
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hConsole, dwMode);
 }
